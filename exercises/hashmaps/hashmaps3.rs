37d95563058d87ea64e364a329cd21d07899c612
andyli233 <andyli2022@163.com>
2025-11-05 01:12:43 -0500

    Half completed

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -39,6 +38,28 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        // let mut scored_1: Team = scores.get(&team_1_name).copied().unwrap_or( Team);
+        // let scored_1 = scored_1 + team_1_score;
+        // scored.insert(team_1_name, team_1_score);
+        // let mut scored_2: u8 = scores.get(&team_2_name).copied().unwrap_or(0);
+        // let scored_2 = scored_2 + team_2_score;
+        // scored.insert(team_2_name, team_2_score);
+        let team_1 = scores.entry(team_1_name).or_insert(Team{goals_scored: 0, goals_conceded: 0});
+        team_1.goals_scored += team_1_score;
+        team_1.goals_conceded += team_2_score;
+        // match scores.entry(team_2_name) {
+        //     Entry::Occupied(mut team) => {
+        //         team.get_mut().goals_scored += team_2_score;
+        //         team.get_mut().goals_conceded += team_1_score;
+        //     }
+        //     Entry::vacant(team) => {
+        //         team.get_mut().goals_scored = team_2_score;
+        //         team.get_mut().goals_conceded = team_1_score;
+        //     }
+        // }
+        let team_2 = scores.entry(team_2_name).or_insert(Team{goals_scored: 0, goals_conceded: 0});
+        team_2.goals_scored += team_2_score;
+        team_2.goals_conceded += team_1_score;
     }
     scores
 }
--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,24 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    String::from(input.trim())
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let s1 = " world!";
+    let mut s2 = String::from(input);
+    s2.push_str(s1);
+    s2
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let mut s = String::from(input);
+    s.replace("cars", "balloons")
 }
 
 #[cfg(test)]
